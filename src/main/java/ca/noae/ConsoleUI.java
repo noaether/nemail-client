7280defe3dc153d05dbb377c579dbca570cd1401
noadev <67566122+thenoadev@users.noreply.github.com>
2023-03-04 14:59:52 -0500

    added createTable() method to show emails

--- src/main/java/ca/noae/ConsoleUI.java
+++ src/main/java/ca/noae/ConsoleUI.java
@@ -1,6 +1,10 @@
 package ca.noae;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Arrays;
+
+import javax.swing.*;
 
 public class ConsoleUI {
   // Clears the console screen
@@ -16,4 +20,42 @@ public class ConsoleUI {
       System.err.println("Failed to clear the console screen: " + ex.getMessage());
     }
   }
+
+  // Function to create and display a scrollable table
+
+  public static void createTable(String[] columnNames, String[][] data) {
+      int[] columnWidths = new int[columnNames.length];
+      for (int i = 0; i < columnNames.length; i++) {
+          columnWidths[i] = columnNames[i].length();
+      }
+      for (String[] row : data) {
+          for (int i = 0; i < row.length; i++) {
+              if (row[i].length() > columnWidths[i]) {
+                  columnWidths[i] = row[i].length();
+              }
+          }
+      }
+      int totalWidth = Arrays.stream(columnWidths).sum() + columnWidths.length - 1;
+      StringBuilder separatorBuilder = new StringBuilder();
+      for (int i = 0; i < totalWidth; i++) {
+          separatorBuilder.append("-");
+      }
+      String separator = separatorBuilder.toString();
+      System.out.println(separator);
+      System.out.print("| ");
+      for (int i = 0; i < columnNames.length; i++) {
+          System.out.printf("%-" + columnWidths[i] + "s | ", columnNames[i]);
+      }
+      System.out.println();
+      System.out.println(separator);
+      for (String[] row : data) {
+          System.out.print("| ");
+          for (int i = 0; i < row.length; i++) {
+              System.out.printf("%-" + columnWidths[i] + "s | ", row[i]);
+          }
+          System.out.println();
+      }
+      System.out.println(separator);
+  }
+
 }

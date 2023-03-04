7a40478ae06bc8657ef1975dc5af00f1baf01602
noadev <67566122+thenoadev@users.noreply.github.com>
2023-03-04 14:44:29 -0500

    added clearScreen() to ConsoleUI

--- src/main/java/ca/noae/ConsoleUI.java
+++ src/main/java/ca/noae/ConsoleUI.java
@@ -1,5 +1,19 @@
 package ca.noae;
 
+import java.io.IOException;
+
 public class ConsoleUI {
-  
+  // Clears the console screen
+  public static void clearScreen() {
+    try {
+      if (System.getProperty("os.name").contains("Windows")) {
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+      } else {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+      }
+    } catch (IOException | InterruptedException ex) {
+      System.err.println("Failed to clear the console screen: " + ex.getMessage());
+    }
+  }
 }

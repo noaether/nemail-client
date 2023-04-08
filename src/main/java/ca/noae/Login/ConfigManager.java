2b3d959cf9019e8c0423c4490e5aa027be3f27ef
noaether <67566122+noaether@users.noreply.github.com>
2023-04-08 14:35:29 -0400

    s6

--- src/main/java/ca/noae/Login/ConfigManager.java
+++ src/main/java/ca/noae/Login/ConfigManager.java
@@ -12,7 +12,7 @@ public class ConfigManager {
   private static String fileName;
 
   public ConfigManager(Scanner scanner, String fileName) {
-    ConfigManager.scanner = scanner;
+    
     ConfigManager.props = new Properties();
     ConfigManager.fileName = fileName;
     try (FileInputStream fis = new FileInputStream(fileName)) {
@@ -24,12 +24,12 @@ public class ConfigManager {
 
   public static String getPropOrQuery(String prop) { // TODO : Implement this method
     if (props == null) {
-      System.out.print("Enter your " + prop + " :");
+      System.out.print("Enter your " + prop + " : ");
       return scanner.nextLine();
     } else {
       String configOrQuery = props.getProperty(prop);
       System.out.print(
-        configOrQuery == null ? "Enter your " + prop + " :" : "Loading " + prop + " from config file... \n");
+        configOrQuery == null ? "Enter your " + prop + " : " : "Loading " + prop + " from config file... \n");
       if (configOrQuery == null) {
         return scanner.nextLine();
       }
@@ -39,12 +39,12 @@ public class ConfigManager {
 
   public static String getPropOrQuery(String prop, String message) { // TODO : Implement this method
     if (props == null) {
-      System.out.print("Enter your " + prop + " :");
+      System.out.print(message);
       return scanner.nextLine();
     } else {
       String configOrQuery = props.getProperty(prop);
       System.out.print(
-        configOrQuery == null ? "Enter your " + prop + " :" : message + "\n");
+        configOrQuery == null ? message : "Loading " + prop + " from config file... \n");
       if (configOrQuery == null) {
         return scanner.nextLine();
       }

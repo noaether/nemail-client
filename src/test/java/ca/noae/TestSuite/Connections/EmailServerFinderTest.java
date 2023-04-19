0dcae8a334f8d3074cfd69c0bfcc3ed83f593172
noaether <67566122+noaether@users.noreply.github.com>
2023-04-18 22:17:44 -0400

    javadocs

--- src/test/java/ca/noae/TestSuite/Connections/EmailServerFinderTest.java
+++ src/test/java/ca/noae/TestSuite/Connections/EmailServerFinderTest.java
@@ -8,6 +8,11 @@ import static org.junit.Assert.assertThrows;
 
 public class EmailServerFinderTest {
 
+    /**
+     * Tests the {@link EmailServerFinder#check(String)} method.
+     *
+     * @throws Exception if the email is invalid
+     */
     @Test
     public void testCheckValidEmail() throws Exception {
         String email = "john.doe@gmail.com";
@@ -17,6 +22,9 @@ public class EmailServerFinderTest {
         assertNotNull(servers[2]);
     }
 
+    /**
+     * Tests the {@link EmailServerFinder#check(String)} method.
+     */
     @Test
     public void testCheckInvalidEmail() {
         String email = "invalid-email";
@@ -25,38 +33,56 @@ public class EmailServerFinderTest {
         });
     }
 
+    /**
+     * Tests the {@link EmailServerFinder#probePorts(String[], String[])} method
+     * with a valid email.
+     *
+     * This test will fail if the email server is down, or if it's unreachable.
+     */
     @Test
     public void testProbePorts() {
-        String[] possibleHosts = {"mail.migadu.com", "smtp.migadu.com"};
-        String[] ports = {"25", "465", "587"};
+        String[] possibleHosts = { "mail.migadu.com", "smtp.migadu.com" };
+        String[] ports = { "25", "465", "587" };
         String result = EmailServerFinder.probePorts(possibleHosts, ports);
         assertNotNull(result);
     }
 
+    /**
+     * Tests the {@link EmailServerFinder#probeCapabilities(String[], String[], String)}
+     * method with SMTP.
+     */
     @Test
     public void testProbeCapabilitiesSMTP() {
-        String[] possibleHosts = {"smtp-mail.outlook.com"};
-        String[] ports = {"25", "465", "587" };
+        String[] possibleHosts = { "smtp-mail.outlook.com" };
+        String[] ports = { "25", "465", "587" };
         String protocol = "SMTP";
         String result = EmailServerFinder.probeCapabilities(possibleHosts, ports, protocol);
         System.out.println(result);
         assertNotNull(result);
     }
 
+    /**
+     * Tests the {@link EmailServerFinder#probeCapabilities(String[], String[], String)}
+     * method with IMAP.
+     */
     @Test
     public void testProbeCapabilitiesIMAP() {
-        String[] possibleHosts = {"imap-mail.outlook.com"};
-        String[] ports = {"143", "993"};
+        String[] possibleHosts = { "imap-mail.outlook.com" };
+        String[] ports = { "143", "993" };
         String protocol = "IMAP";
         String result = EmailServerFinder.probeCapabilities(possibleHosts, ports, protocol);
         System.out.println(result);
         assertNotNull(result);
     }
 
+    /**
+     * Tests the {@link EmailServerFinder#probeCapabilities(String[], String[], String)}
+     * method with POP3.
+     */
     @Test
     public void testProbeCapabilitiesPOP3() {
-        String[] possibleHosts = {"pop-mail.outlook.com"};
-        String[] ports = {"110", "995"};
+        String[] possibleHosts = { "pop-mail.outlook.com" };
+        String[] ports = { "110", "995" };
         String protocol = "POP3";
         String result = EmailServerFinder.probeCapabilities(possibleHosts, ports, protocol);
         System.out.println(result);

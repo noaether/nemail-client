0af502a983e407184fee1920c51491f313609db3
noaether <67566122+noaether@users.noreply.github.com>
2023-04-18 22:19:18 -0400

    too much weird {} in this file

--- src/test/java/ca/noae/TestSuite/Connections/EmailServerFinderTest.java
+++ src/test/java/ca/noae/TestSuite/Connections/EmailServerFinderTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThrows;
 
+@SuppressWarnings("checkstyle:nowhitespaceafter")
 public class EmailServerFinderTest {
 
     /**

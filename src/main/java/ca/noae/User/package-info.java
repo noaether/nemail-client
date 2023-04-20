2c5714f05be25fcada4a8967bb3eb441e8663cab
noaether <67566122+noaether@users.noreply.github.com>
2023-04-20 13:55:24 -0400

    fixed lack of javadoc

--- src/main/java/ca/noae/User/package-info.java
+++ src/main/java/ca/noae/User/package-info.java
@@ -1,1 +1,4 @@
+/**
+ * This package contains classes associated with User experience. (I'm not actually sure why I have this package)
+ */
 package ca.noae.User;
--- src/main/java/ca/noae/package-info.java
+++ src/main/java/ca/noae/package-info.java
@@ -1,1 +1,7 @@
+/**
+ * This package contains the classes that are used to run the application.
+ *
+ * @since the beginning of time
+ * @version 1.0
+ */
 package ca.noae;

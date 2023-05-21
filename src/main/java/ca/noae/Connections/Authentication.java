d31b4142c2fd14b9aa7a8bd43ea24bd3d2c2c06b
noaether <67566122+noaether@users.noreply.github.com>
2023-05-21 12:14:32 -0400

    fixes issue #10

--- src/main/java/ca/noae/Connections/Authentication.java
+++ src/main/java/ca/noae/Connections/Authentication.java
@@ -49,6 +49,7 @@ public final class Authentication {
   public static Transport getSMTPTransport() throws MessagingException {
     Properties props = new Properties();
     props.put("mail.smtp.auth", "true");
+    props.put("mail.smtp.starttls.enable", "true");
     props.put("mail.smtp.socketFactory.class",
         "javax.net.ssl.SSLSocketFactory");
     props.put("mail.smtp.host", user.getSmtpServerAddress());
